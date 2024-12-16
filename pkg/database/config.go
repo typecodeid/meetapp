2d23261cb71b1ff6d9f282e2f99b8073f738d081
prasojopxl <prasojodesigner@gmail.com>
2024-12-16 15:01:33 +0700

    connect railway database

--- pkg/database/config.go
+++ pkg/database/config.go
@@ -11,12 +11,12 @@ import (
 var DB *sql.DB
 
 // ConnectDB menginisialisasi koneksi database dan menyimpannya di variabel global DB
-func ConnectDB(user, password, dbname string) (*sql.DB, error) {
+func ConnectDB(user, password, dbname string, host string, port string) (*sql.DB, error) {
 	if DB != nil {
 		return DB, nil
 	}
 
-	psqlInfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+	psqlInfo := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable", user, password, dbname, host, port)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		return nil, err
